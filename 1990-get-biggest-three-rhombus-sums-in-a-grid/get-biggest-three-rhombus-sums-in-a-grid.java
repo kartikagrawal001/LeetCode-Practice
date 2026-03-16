134a9e9f0c3d01a6171dc91c1a98a3b51e59fbb5
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-03-16 15:44:00 +0530

    Time: 17 ms (98.53%) | Memory: 47.2 MB (83.82%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -1,49 +1,56 @@
-import java.util.*;
-
 class Solution {
-    public int[] getBiggestThree(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                set.add(grid[i][j]);
-
-                for (int k = 1; i - k >= 0 && i + k < m && j - k >= 0 && j + k < n; k++) {
-
-                    int sum = 0;
-
-                    int r = i - k, c = j;
-
-                    for (int t = 0; t < k; t++)
-                        sum += grid[r + t][c + t];
-
-                    for (int t = 0; t < k; t++)
-                        sum += grid[r + k + t][c + k - t];
-
-                    for (int t = 0; t < k; t++)
-                        sum += grid[r + 2 * k - t][c - t];
-
-                    for (int t = 0; t < k; t++)
-                        sum += grid[r + k - t][c - k + t];
-
-                    set.add(sum);
+            public int[] getBiggestThree(int[][] grid) {
+        int x = grid.length-1, y = grid[0].length-1;
+        int[] res = new int[3];
+        for(int i=0; i<=x; i++){
+            for(int j=0; j<=y; j++){
+                int left = Math.min(x, j);
+                int right = Math.min(x, y-j);
+                int down = (x-i)/2;
+                int radius = Math.min(Math.min(left, down), right);
+                fillTop(res, grid[i][j]);
+                for(int k=1; k<=radius; k++){
+                    findRadius(grid, k, i, j, res);
                 }
+
             }
         }
-
-        int size = Math.min(3, set.size());
-        int[] ans = new int[size];
-        int idx = 0;
-
-        for (int val : set) {
-            if (idx == size) break;
-            ans[idx++] = val;
+        if(res[2]>0){
+            return res;
+        } if(res[1]>0){
+            return new int[]{res[0], res[1]};
+        } else {
+            return new int[]{res[0]};
         }
+    }
 
-        return ans;
+    private void findRadius(int[][] grid, int k, int i, int j, int[] res) {
+        int b = 2*(k);
+        int leftSum = 0, rightSum=0, leftbtm=0, rightbtm=0, up = grid[i][j], down = grid[b+i][j];
+        for(int m=1; m<=k; m++){
+            leftSum = leftSum + grid[i+m][j-m];
+            rightSum = rightSum + grid[i+m][j+m];
+            if(i+m != (b+i)-m) {
+                leftbtm = leftbtm + grid[b+i - m][j - m];
+            }
+            if(i+m!=b+i-m) {
+                rightbtm = rightbtm + grid[b+i - m][j + m];
+            }
+        }
+        int sum = leftSum+rightSum+leftbtm+rightbtm+up+down;
+        fillTop(res, sum);
+    }
+
+    private void fillTop(int[] res, int val) {
+        if(val!=res[0] && val!=res[1] && val!=res[2] && val>res[0]){
+            res[2] = res[1];
+            res[1] = res[0];
+            res[0] = val;
+        } else if(val!=res[0] && val!=res[1] && val!=res[2] && val>res[1]){
+            res[2] = res[1];
+            res[1] = val;
+        } else if(val!=res[0] && val!=res[1] && val!=res[2] && val>res[2]){
+            res[2] = val;
+        }
     }
 }
