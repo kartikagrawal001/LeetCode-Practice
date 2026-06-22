5e598090ac286247a4242e1a61e62215779de484
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-06-22 12:25:06 +0530

    Time: 2 ms (96.67%) | Memory: 43.5 MB (54.89%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,23 +1,19 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char c:text.toCharArray())
-            map.put(c,map.getOrDefault(c,0)+1);
-        String b = "balloon";
-        int count = 0;
-        boolean f = true;
-        while(f){
-            for(char c:b.toCharArray()){
-                if(map.containsKey(c)){
-                    map.put(c,map.getOrDefault(c,0)-1);
-                    if(map.get(c)==0)map.remove(c);
-                }
-                else {
-                    f = false;
-                }
-            }
-            if(f)count++;
+        int b = 0;
+        int a = 0;
+        int l = 0;
+        int o = 0;
+        int n = 0;
+        for(char ch:text.toCharArray()){
+            if(ch=='b')b++;
+            else if(ch=='a')a++;
+            else if(ch=='l')l++;
+            else if(ch=='o')o++;
+            else if(ch=='n')n++;
         }
-        return count;
+        l/=2;
+        o/=2;
+        return Math.min(Math.min(b,a),Math.min(Math.min(l,o),Math.min(o,n)));
     }
 }
