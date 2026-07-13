cf8f1d1cdc1ab51fb5f631ac4923fc1c471c9f99
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-07-13 22:45:07 +0530

    Time: 15 ms (32.40%) | Memory: 47.3 MB (7.74%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c:magazine.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:ransomNote.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c,map.getOrDefault(c,0)-1);
+                if(map.get(c)==0)map.remove(c);
+            }
+            else return false;
+        }
+        return true;
+    }
+}
