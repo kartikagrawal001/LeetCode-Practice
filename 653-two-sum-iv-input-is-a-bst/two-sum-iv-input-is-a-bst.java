c17eccf55b5da37ade5025baafeddae9d7ce3631
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-07-31 01:40:34 +0530

    Time: 3 ms (73.07%) | Memory: 47.4 MB (21.33%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,11 +14,48 @@
  * }
  */
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
+    Stack<TreeNode> asc = new Stack<>();
+    Stack<TreeNode> dsc = new Stack<>();
     public boolean findTarget(TreeNode root, int k) {
         if(root==null)return false;
-        if(set.contains(k-root.val))return true;
-        set.add(root.val);
-        return findTarget(root.left,k)||findTarget(root.right,k);
+        TreeNode t = root;
+        while(t!=null){
+            asc.push(t);
+            t = t.left;
+        }
+        t = root;
+        while(t!=null){
+            dsc.push(t);
+            t = t.right;
+        }
+        TreeNode i = getSmall();
+        TreeNode j = getBig();
+        while(i!=null && j!=null && i!=j && i.val<j.val ){
+            int sum = i.val+j.val;
+            if(sum==k)return true;
+            else if(sum>k)j=getBig();
+            else i = getSmall();
+        }
+        return false;
+    }
+    public TreeNode getSmall(){
+        if(asc.isEmpty())return null;
+        TreeNode small = asc.pop();
+        TreeNode right = small.right;
+        while(right!=null){
+            asc.push(right);
+            right = right.left;
+        }
+        return small;
+    }
+    public TreeNode getBig(){
+        if(dsc.isEmpty())return null;
+        TreeNode big = dsc.pop();
+        TreeNode left = big.left;
+        while(left!=null){
+            dsc.push(left);
+            left = left.right;
+        }
+        return big;
     }
 }
