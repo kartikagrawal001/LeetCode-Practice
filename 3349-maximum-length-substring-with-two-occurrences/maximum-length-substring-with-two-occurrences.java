bae0ed44fb0993266b8a9d4a99c2cb3811f7aa55
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-08-15 03:30:48 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (37.06%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        Map<Character,Integer> map =  new HashMap<>();
+        int arr[] = new int[256];
         int low = 0;
         int ans = 0;
         for(int high = 0;high<s.length();high++){
             char ch = s.charAt(high);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            if(map.get(ch)>2){
-                while(map.get(ch)>2){
+            arr[ch]++;
+            if(arr[ch]>2){
+                while(arr[ch]>2){
                     char ch2 = s.charAt(low);
                     low++;
-                    map.put(ch2,map.getOrDefault(ch2,0)-1);
-                    if(map.get(ch2)==0)map.remove(ch2);
-
+                    arr[ch2]--;
                 }
             }
             ans = Math.max(ans,high-low+1);
