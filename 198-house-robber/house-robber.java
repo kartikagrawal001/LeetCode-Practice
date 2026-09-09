218e8034fa7e9bf5b1b1c918a02f3f823c84cd5f
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-09-09 16:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (56.39%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -5,15 +5,14 @@ class Solution {
             dp[i][0] = -1;
             dp[i][1] = -1;
         }
-        int free = 1;
-        return helper(0,free,nums,dp); 
+        return helper(0,1,nums,dp);
     }
     public int helper(int i,int free,int[]nums,int[][]dp){
         if(i==nums.length)return 0;
         if(dp[i][free]!=-1)return dp[i][free];
         if(free==0)return helper(i+1,1,nums,dp);
-        int c1 = nums[i] + helper(i+1,0,nums,dp);
-        int c2 = helper(i+1,1,nums,dp);
-        return dp[i][free] = Math.max(c1,c2);
+        int take = nums[i] + helper(i+1,0,nums,dp);
+        int skip = helper(i+1,1,nums,dp);
+        return dp[i][free] = Math.max(take,skip);
     }
 }
