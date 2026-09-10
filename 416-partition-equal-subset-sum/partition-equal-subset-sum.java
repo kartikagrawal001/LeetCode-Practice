e1efa8c5a259877c8db1eace08670c1b057ab3d4
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-09-10 12:52:49 +0530

    Time: 51 ms (46.66%) | Memory: 47.8 MB (48.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,21 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int total = 0;
-        for(int v:nums)total+=v;
-        if(total%2!=0)return false;
-        Boolean dp[][] = new Boolean[nums.length][total+1];
-        return helper(0,total/2,nums,dp);
-    }
-    public boolean helper(int i,int sum,int[]nums,Boolean dp[][]){
-        if(i==nums.length){
-            if(sum==0)return true;
-            else return false;
+        for (int v : nums) total += v;
+        if (total % 2 != 0) return false;
+        int target = total / 2;
+        boolean dp[][] = new boolean[nums.length + 1][target + 1];
+        for (int i = 0; i <= nums.length; i++) {
+            dp[i][0] = true;
         }
-        if(dp[i][sum]!=null)return dp[i][sum];
-        if(nums[i]>sum)return dp[i][sum] = helper(i+1,sum,nums,dp);
-        boolean take = helper(i+1,sum-nums[i],nums,dp);
-        boolean skip = helper(i+1,sum,nums,dp);
-        return dp[i][sum] = take||skip;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int sum = 1; sum <= target; sum++) {
+                if (nums[i] > sum)
+                    dp[i][sum] = dp[i + 1][sum];
 
+                else {
+                    boolean take = dp[i + 1][sum - nums[i]];
+                    boolean skip = dp[i + 1][sum];
+
+                    dp[i][sum] = take || skip;
+                }
+            }
+        }
+        return dp[0][target];
     }
 }
