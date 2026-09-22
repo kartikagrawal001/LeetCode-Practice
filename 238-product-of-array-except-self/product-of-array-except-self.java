3d6a0e7b9ae6bab9b57adfae7526e67bb236c959
kartikagrawal001 <142593080+kartikagrawal001@users.noreply.github.com>
2026-09-22 09:01:35 +0530

    Time: 3 ms (23.23%) | Memory: 64.9 MB (87.76%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int prefix []= new int[nums.length];
-        prefix[0] = 1;
-        int suffix []= new int[nums.length];
-        suffix[nums.length-1] = 1;
+        int []prepro = new int[nums.length];
+        prepro[0]=1;
         for(int i=1;i<nums.length;i++){
-            prefix[i] = prefix[i-1]*nums[i-1];
+            prepro[i] = prepro[i-1]*nums[i-1];
         }
-        for(int i = nums.length-2;i>=0;i--){
-            suffix[i] = suffix[i+1]*nums[i+1];
+        int []suffpro = new int[nums.length];
+        suffpro[nums.length-1]=1;
+        for(int i=nums.length-2;i>=0;i--){
+            suffpro[i] = suffpro[i+1]*nums[i+1];
         }
-        int ans[] = new int[nums.length];
+        int[]ans = new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            ans[i] = prefix[i]*suffix[i];
+            ans[i] = prepro[i]*suffpro[i];
         }
         return ans;
     }
